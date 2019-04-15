b627d69d9256dc9c874795908a9d9fe8ada657a5
Dariusz Okonski <darek200180@gmail.com>
2019-04-15 12:53:14 +0100

    Updates

---
+++ precache-manifest.371293339a61d45a1ebe74dcaaf1ad27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdf27039b438a24f9287",
+    "url": "/react-simple-shopping-cart-006/static/js/runtime~main.c8762701.js"
+  },
+  {
+    "revision": "989fb4a5e22fd8a75b45",
+    "url": "/react-simple-shopping-cart-006/static/js/main.3d59ccf8.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-simple-shopping-cart-006/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "989fb4a5e22fd8a75b45",
+    "url": "/react-simple-shopping-cart-006/static/css/main.25e9093a.chunk.css"
+  },
+  {
+    "revision": "fdd4ff71d0f9e4c417fb09f8c4d67df7",
+    "url": "/react-simple-shopping-cart-006/index.html"
+  }
+];
